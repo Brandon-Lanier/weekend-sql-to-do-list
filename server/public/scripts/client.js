c6b1f8103eaad6c9f6966274b57683a932be72fd
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-10 22:02:32 -0600

    Added a post function to handle new tasks

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,18 +1,60 @@
 $(document).ready(onReady);
 
 function onReady() {
-    $('#submitBtn').on('click', addTask)
+    $('#submitBtn').on('click', taskSubmit)
     getTasks();
 }
 
 function getTasks() {
+    $('.inputs').val('');
     $.ajax({
         method: 'GET',
         url: '/tasks'
     }).then(response => {
         console.log('Got Tasks', response);
-        renderTasks();
+        renderTasks(response);
     }).catch(error => {
         console.log('Error Getting Tasks', error);   
     })
 }
+
+function renderTasks(res) {
+    $('#tableBody').empty();
+    for (let task of res) {
+        $('#tableBody').append(`
+            <tr data-id=${task.id}>
+                <td>${task.task}</td>
+                <td>${task.notes}</td>
+                <td>${task.priority}</td>
+                <td><span class="completed"></span><button class="completeBtn" data-id=${task.id}>Mark Completed</button></td>
+                <td><button class="deleteBtn" data-id=${task.id}>Delete</button>
+            </tr>
+        `);
+    }
+}
+
+function taskSubmit() {
+    let newTask = {};
+        newTask.task = $('#taskIn').val(),
+        newTask.notes = $('#notesIn').val(),
+        newTask.priority = $('#prioritySel').val();
+        console.log(newTask);
+        addTask(newTask);
+        // $('.inputs').val('');
+}
+
+function addTask(taskIn) {
+    console.log(taskIn);
+    $.ajax({
+        method: 'POST',
+        url: '/tasks',
+        data: taskIn
+    }).then(function(response){
+        getTasks();
+    }).catch(function(error) {
+        console.log('Error Adding Task', error);
+        alert('Unable To Add Task')
+    })
+}
+
+function
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,7 +2,32 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "tasks" ORDER BY "priority";`;
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('Error getting data', error);
+        res.sendStatus(500)
+    })
+})
 
+router.post('/', (req, res) => {
+    let newTask = req.body;
+    let queryText = `
+        INSERT INTO "tasks" ("task", "notes", "priority")
+        VALUES ($1, $2, $3);`;
+        pool.query(queryText, [newTask.task, newTask.notes, newTask.priority])
+        .then(result => {
+            res.sendStatus(201);
+        }).catch(err => {
+            res.sendStatus(500)
+        })
+})
+
+router.delete('/:id' (req, res) {
+    let taskId = req.params.id
+})
 
 
 
