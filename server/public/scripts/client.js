7a0a7aa4d425aca2b578eae888e4ebb4a3da8f79
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-10 19:51:26 -0600

    Set up Dev Environment

---
+++ server/public/scripts/client.js
@@ -0,0 +1,6 @@
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('JQ synced');
+    
+}
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const booksRouter = require('./routes/book.router.js');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended}));
+
+app.use(express.static('server/public'))
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
